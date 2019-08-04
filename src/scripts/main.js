e5f9972d4a69980208862fe149850e095c411184
nik787 <nikito101d@gmail.com>
2019-08-04 19:56:54 +0300

    Добавил навигацию в отзывах

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,17 @@
-$('.feedbacks__list').owlCarousel({
-  loop:true,
-  margin:30,
-  nav:true,
-  navText: [`<i class="prev">Prev</i>`,`<i class="next">next</i>`],
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:1
-      },
-      1000:{
-          items:1
-      }
-  }
+$(".navigation__burger").on("click", function() {
+    $(".navigation__burger").toggleClass("is-active");
+    $(".navigation__list").toggleClass("active");
+
+});
+$(".navigation__list").on("click","a", function (event) {
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+     
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
 });
 $('.catalog__list').owlCarousel({
     loop:true,
@@ -37,23 +35,26 @@ $('.catalog__list').owlCarousel({
         }
     }
 })
-$(".navigation__list").on("click","a", function (event) {
-    //забираем идентификатор бока с атрибута href
-    var id  = $(this).attr('href'),
-
-    //узнаем высоту от начала страницы до блока на который ссылается якорь
-        top = $(id).offset().top;
-     
-    //анимируем переход на расстояние - top за 1500 мс
-    $('body,html').animate({scrollTop: top}, 1500);
+$('.feedbacks__list').owlCarousel({
+  loop:true,
+  margin:30,
+  nav:true,
+  navText: [`<img class="prev" src="./images/right-arrow.svg">`,`<img class="next" src="./images/right-arrow.svg">`],
+  responsive:{
+      0:{
+          items:1,
+          nav:false
+      },
+      600:{
+          items:1,
+          nav:false
+      },
+      1000:{
+          items:1
+      }
+  }
 });
-
 $('#form2-tel').on("click", function() {
     $('#form2-tel').attr({"value":"+7"})
 })
 
-$(".navigation__burger").on("click", function() {
-    $(".navigation__burger").toggleClass("is-active");
-    $(".navigation__list").toggleClass("active");
-
-})
