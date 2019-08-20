589e614aabb480de5e6cdb576df78d9e8d4a6351
nik787 <nikito101d@gmail.com>
2019-08-20 12:07:45 +0300

    адаптивный шрифт, сменил px на em и rem

--- smartgrid.js
+++ smartgrid.js
@@ -4,25 +4,25 @@ const smartgrid = require('smart-grid');
 const settings = {
     outputStyle: 'less', /* less || scss || sass || styl */
     columns: 12, /* number of grid columns */
-    offset: '30px', /* gutter width px || % || rem */
+    offset: '1.875em', /* gutter width px || % || rem */
     mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
     container: {
-        maxWidth: '1600px', /* max-width оn very large screen */
-        fields: '30px' /* side fields */
+        maxWidth: '100em', /* max-width оn very large screen */
+        fields: '1.875em' /* side fields */
     },
     breakPoints: {
         lg: {
-            width: '1100px', /* -> @media (max-width: 1100px) */
+            width: '68.75em', /* -> @media (max-width: 1100px) */
         },
         md: {
-            width: '960px'
+            width: '60em'
         },
         sm: {
-            width: '780px',
-            fields: '15px' /* set fields only if you want to change container.fields */
+            width: '48.75em',
+            fields: '1em' /* set fields only if you want to change container.fields */
         },
         xs: {
-            width: '560px'
+            width: '35em'
         }
         /* 
         We can create any quantity of break points.
